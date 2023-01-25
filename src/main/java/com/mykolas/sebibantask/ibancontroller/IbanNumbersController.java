bf6da3f52f1fb2bc1d53e3b8013ec138a6850770
Mykolas <mykolas.kucinskas2@gmail.com>
2023-01-26 00:03:03 +0200

    changed List to HashMap in PostMapping, added documentation in controller

--- src/main/java/com/mykolas/sebibantask/ibancontroller/IbanNumbersController.java
+++ src/main/java/com/mykolas/sebibantask/ibancontroller/IbanNumbersController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController("iban")
 public class IbanNumbersController {
@@ -21,21 +23,31 @@ public class IbanNumbersController {
         this.ibanValidationService = ibanValidationService;
     }
 
+    /**
+     * @author Mykolas
+     * @param ibanNumber The single provided IBAN number by the user.
+     * @return boolean value true if IBAN has been successfully validated.
+     */
     @PostMapping("single")
     public boolean validateSingleIbanNumber(String ibanNumber) {
        return ibanValidationService.validateSingleIbanNumber(ibanNumber);
     }
 
+    /**
+     * @author Mykolas
+     * @param listOfIbans The Object with the list of IBAN numbers provided by the user.
+     * @return Returns a HashMap with IBAN as a key and boolean value if IABN is valid as a value in HashMap.
+     */
     @PostMapping("multiple")
-    public List<Boolean> validateListOfIbans(@RequestBody ListOfIbans listOfIbans) {
+    public Map<String,Boolean> validateListOfIbans(@RequestBody ListOfIbans listOfIbans) {
 
-        List<Boolean> validateListOfIbans = new ArrayList<>();
+        Map<String,Boolean> validatePairsOfIbans = new HashMap<>();
 
         for(String iban : listOfIbans.getIbans()){
             Boolean validationOfIban = ibanValidationService.validateSingleIbanNumber(iban);
-            validateListOfIbans.add(validationOfIban);
+            validatePairsOfIbans.put(iban,validationOfIban);
         }
 
-        return validateListOfIbans;
+        return validatePairsOfIbans;
     }
 }
