684b932ddc604a7df7396539b26d1c3c391765b6
Mykolas <mykolas.kucinskas2@gmail.com>
2023-01-25 19:55:59 +0200

    added validation for IBAN length by country code

--- src/main/java/com/mykolas/sebibantask/ibanservice/IbanValidationService.java
+++ src/main/java/com/mykolas/sebibantask/ibanservice/IbanValidationService.java
@@ -1,16 +1,25 @@
 package com.mykolas.sebibantask.ibanservice;
 
 
+import com.mykolas.sebibantask.helper.IbanCodeLengthList;
 import com.mykolas.sebibantask.ibanexceptions.IncorrectIbanLengthException;
 import com.mykolas.sebibantask.ibanexceptions.NoSuchCountryCodeException;
 import com.neovisionaries.i18n.CountryCode;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 
 @Service
 public class IbanValidationService {
+
+    private IbanCodeLengthList ibanCodeLengthList;
+
+    public IbanValidationService(IbanCodeLengthList ibanCodeLengthList){
+        this.ibanCodeLengthList = ibanCodeLengthList;
+    }
 
 
     public void validateSingleIbanNumber(String ibanNumber) {
@@ -24,6 +33,11 @@ public class IbanValidationService {
         String countryCodeOfIban = ibanNumber.substring(0,2);
         if(!ISO_COUNTRIES.contains(countryCodeOfIban)){
             throw new NoSuchCountryCodeException(countryCodeOfIban);
+        }
+
+        Map<String,Integer> listOfIbanLengthByCode = ibanCodeLengthList.getIbanLengthByCountry();
+        if (ibanNumber.length() != listOfIbanLengthByCode.get(countryCodeOfIban) ){
+                throw new IncorrectIbanLengthException("Incorrect length of provided Iban according to the country code.");
         }
     }
 }
