59d831a2260e260bb3364974177e7750b54d6f6c
Mykolas <mykolas.kucinskas2@gmail.com>
2023-01-25 17:39:24 +0200

    added controller and service

--- src/main/java/com/mykolas/sebibantask/ibancontroller/IbanNumbersController.java
+++ src/main/java/com/mykolas/sebibantask/ibancontroller/IbanNumbersController.java
@@ -1,5 +1,23 @@
 package com.mykolas.sebibantask.ibancontroller;
 
 
+import com.mykolas.sebibantask.ibanservice.IbanValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IbanNumbersController {
+
+    private final IbanValidationService ibanValidationService;
+
+    @Autowired
+    public IbanNumbersController(IbanValidationService ibanValidationService){
+        this.ibanValidationService = ibanValidationService;
+    }
+
+    @PostMapping
+    public void validateSingleIbanNumber(String ibanNumber) {
+        ibanValidationService.validateSingleIbanNumber();
+    }
 }
---
+++ src/main/java/com/mykolas/sebibantask/ibanservice/IbanValidationService.java
@@ -0,0 +1,12 @@
+package com.mykolas.sebibantask.ibanservice;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IbanValidationService {
+
+    public void validateSingleIbanNumber() {
+
+    }
+}
