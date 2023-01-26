acda62c35fc1cb2720fb78670da1ac669027b62c
Mykolas <mykolas.kucinskas2@gmail.com>
2023-01-26 11:33:28 +0200

    added junit tests to throw exceptions in IbanValidationService

--- src/test/java/com/mykolas/sebibantask/ibanservice/IbanValidationServiceTest.java
+++ src/test/java/com/mykolas/sebibantask/ibanservice/IbanValidationServiceTest.java
@@ -1,4 +1,44 @@
 package com.mykolas.sebibantask.ibanservice;
 
-public class IbanValidationServiceTest {
+import com.mykolas.sebibantask.helper.IbanCodeLengthList;
+import com.mykolas.sebibantask.helper.LettersToNumberConverterList;
+import com.mykolas.sebibantask.ibanexceptions.IncorrectIbanLengthException;
+import com.mykolas.sebibantask.ibanexceptions.NoSuchCountryCodeException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(classes = {IbanValidationService.class, IbanCodeLengthList.class, LettersToNumberConverterList.class})
+class IbanValidationServiceTest {
+
+
+    private IbanValidationService ibanValidationService;
+
+    @Mock
+    private IbanCodeLengthList ibanCodeLengthList;
+
+    @Mock
+    private LettersToNumberConverterList lettersToNumberConverterList;
+
+    @BeforeEach
+    void init() {
+        ibanValidationService = new IbanValidationService(ibanCodeLengthList, lettersToNumberConverterList);
+    }
+
+    @Test
+    void should_return_IncorrectIbanLengthException () {
+        String incorrectLengthIbanTooShort = "BA85" ;
+        assertThrows(IncorrectIbanLengthException.class, () -> ibanValidationService.validateSingleIbanNumber(incorrectLengthIbanTooShort));
+    }
+
+    @Test
+    void should_return_NoSuchCountryCodeException() {
+        String incorrectCountryCodeIbanNumber = "AA47892255255";
+        assertThrows(NoSuchCountryCodeException.class, () -> ibanValidationService.validateSingleIbanNumber(incorrectCountryCodeIbanNumber));
+    }
 }
